b30527fc1c9bd45c6bd5e4ffe7d7e1ce095bca8e
Andy Allard <109981829+andyallard@users.noreply.github.com>
2024-02-23 16:38:44 -0800

    Add html, css, and js files and set them up with the basics.

---
+++ script.js
@@ -0,0 +1,27 @@
+let TOTAL_SIZE = window.innerHeight;
+let GRID_SIZE = 16;
+let BOX_SIZE = Math.floor(TOTAL_SIZE / GRID_SIZE);
+
+let console = document.querySelector('.console');
+
+function setup() {
+    const grid = document.querySelector('.grid');
+    for (let i = 0; i < GRID_SIZE; i++) {
+        row = document.createElement('div');
+        row.classList.add('gridrow');
+
+        // Create all the boxes for current row
+        for (let j = 0; j < GRID_SIZE; j++) {
+            square = document.createElement('div');
+            square.style.width = `${BOX_SIZE}px`;
+            square.style.height = `${BOX_SIZE}px`;
+            square.classList.add('square');
+            row.appendChild(square);
+        }
+
+        grid.appendChild(row);
+    }
+}
+
+setup();
+console.textContent = BOX_SIZE;
