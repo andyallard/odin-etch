88c5b3c2576fb2b3569732e735ced03c2d941722
Andy Allard <109981829+andyallard@users.noreply.github.com>
2024-02-23 17:36:07 -0800

    Add ability to change grid size

--- script.js
+++ script.js
@@ -16,6 +16,17 @@ function setup() {
             square.style.width = `${BOX_SIZE}px`;
             square.style.height = `${BOX_SIZE}px`;
             square.classList.add('square');
+            if (i === 0 && j === 0) {
+                settings = document.createElement('p');
+                settings.classList.add('settings');
+                // settings.textContent = '?';
+                settings.style.fontSize = `${BOX_SIZE - 4}px`;
+                
+                settings.addEventListener('click', changeGridSize);
+
+                square.appendChild(settings);
+            }
+
             row.appendChild(square);
         }
 
@@ -29,6 +40,23 @@ function changeBackground(event) {
     if (event.target.classList.contains('square')) {
         event.target.classList.add('hover');
     }
+}
+
+function changeGridSize() {
+    GRID_SIZE = 0;
+    while (GRID_SIZE > 100 || GRID_SIZE < 2 || isNaN(parseInt(GRID_SIZE))) {
+        GRID_SIZE = window.prompt('Choose a new grid size! For example, if you enter 30, it will create a 30x30 grid.\n\nMIN SIZE = 2, MAX SIZE = 100');
+    }
+    
+    TOTAL_SIZE = window.innerHeight;
+    BOX_SIZE = Math.floor(TOTAL_SIZE / GRID_SIZE);
+
+    div = document.querySelector('.grid');
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+
+    setup();
 }
 
 setup();
