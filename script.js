caf075c8db8b806d85e035aa64b52244bfaf9be7
Andy Allard <109981829+andyallard@users.noreply.github.com>
2024-02-23 23:46:14 -0800

    Change hover to opacity (slow fill) instead of immediate fill.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let TOTAL_SIZE = window.innerHeight;
+let IS_MOBILE = /Mobi|Android/i.test(navigator.userAgent);
+
+IS_MOBILE ? TOTAL_SIZE = window.innerWidth : TOTAL_SIZE = window.innerHeight;
+
 let GRID_SIZE = 16;
 let BOX_SIZE = Math.floor(TOTAL_SIZE / GRID_SIZE);
 
@@ -15,6 +18,7 @@ function setup() {
             square = document.createElement('div');
             square.style.width = `${BOX_SIZE}px`;
             square.style.height = `${BOX_SIZE}px`;
+            square.setAttribute('data-number', '0');
             square.classList.add('square');
             if (i === 0 && j === 0) {
                 settings = document.createElement('p');
@@ -27,21 +31,38 @@ function setup() {
                 square.appendChild(settings);
             }
 
+            // if on mobile, this will add touch listeners to each square.
+            // this impacts performance, so is only done on mobile.
+            if (IS_MOBILE) {
+                square.addEventListener('touchstart', touchChangeBackground);
+                square.addEventListener('touchmove', touchChangeBackground);
+            }
+
             row.appendChild(square);
         }
 
         grid.appendChild(row);
     }
 
-    grid.addEventListener('mouseover', changeBackground);
-    grid.addEventListener('touchstart', changeBackground);
-    grid.addEventListener('touchmove', changeBackground);
+    if (!IS_MOBILE) {
+        grid.addEventListener('mouseover', changeBackground);
+    }
 }
 
 function changeBackground(event) {
     if (event.target.classList.contains('square')) {
-        event.target.classList.add('hover');
+        square = event.target
+        // event.target.classList.add('hover');
+        opacity = Math.min(parseInt(square.getAttribute('data-number')) + 1, 10);
+        square.setAttribute('data-number', opacity);
+        square.style.backgroundColor = `rgba(22, 90, 112, ${opacity / 10})`;
     }
+}
+
+function touchChangeBackground(event) {
+    event.preventDefault();
+    alert(Array(...event.touches));
+    event.target.classList.add('hover');
 }
 
 function changeGridSize() {
@@ -52,6 +73,7 @@ function changeGridSize() {
     
     TOTAL_SIZE = window.innerHeight;
     BOX_SIZE = Math.floor(TOTAL_SIZE / GRID_SIZE);
+    IS_MOBILE = /Mobi|Android/i.test(navigator.userAgent);
 
     div = document.querySelector('.grid');
     while (div.firstChild) {
